f130d1c233d2a7b63d646f331844e0a5af1b1904
Hellowar12345 <144247183+Hellowar12345@users.noreply.github.com>
2025-08-10 11:17:19 +0800

    Update algo3.c

--- algo/algo3.c
+++ algo/algo3.c
@@ -4,17 +4,17 @@
 #include <unistd.h>
 #define Pi 0
 #define Pj 1
-int turn=Pi;
+int turn;
 bool flag[2]={false,false};//flag[0] for Pi, flag[1] for Pj
   //process i
 void *process_i(void *arg) {
    while(1){
     flag[Pi]=true;//我想進去
+    turn=Pj;//互相把鑰匙給對方當兩者都有意願時先射出去的先拿到鑰匙（對方進迴圈前要先射鑰匙）
     while(flag[Pj]&&turn==Pj);//鑰匙在Pj身上且Pj有意願進去
     //critical section
     printf("Process i in critical section\n");
     usleep(500000);//用sleep模擬正在跑這段section的程式
-    turn=Pj;出來後把鑰匙給Pj
     flag[Pi]=false;//出來後改成沒意願
     //remainder section
     usleep(500000);
@@ -24,11 +24,11 @@ void *process_i(void *arg) {
 void *process_j(void *arg) {
   while(1){
     flag[Pj]=true;
+    turn=Pi;
     while(flag[Pi]&&turn==Pi);
     //critical section
     printf("Process j in critical section\n");
     usleep(500000);
-    turn=Pi;
     flag[Pj]=false;
     //remainder section
     usleep(500000);
