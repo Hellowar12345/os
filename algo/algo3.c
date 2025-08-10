10358ee30d6edba15fa45a91ecb9f4e85a105919
Hellowar12345 <144247183+Hellowar12345@users.noreply.github.com>
2025-08-10 11:13:17 +0800

    Update algo3.c

--- algo/algo3.c
+++ algo/algo3.c
@@ -10,11 +10,11 @@ bool flag[2]={false,false};//flag[0] for Pi, flag[1] for Pj
 void *process_i(void *arg) {
    while(1){
     flag[Pi]=true;//我想進去
-    while(flag[Pj]&&turn==Pj);//先等j沒意願（在remainder section）
+    while(flag[Pj]&&turn==Pj);//鑰匙在Pj身上且Pj有意願進去
     //critical section
     printf("Process i in critical section\n");
     usleep(500000);//用sleep模擬正在跑這段section的程式
-    turn=Pj;
+    turn=Pj;出來後把鑰匙給Pj
     flag[Pi]=false;//出來後改成沒意願
     //remainder section
     usleep(500000);
